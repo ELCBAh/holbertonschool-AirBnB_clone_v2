2ed5fafac8dcffe292f917662d8b59abcd5cbb88
ELCBAh <6211@holbertonstudents.com>
2023-08-04 20:43:06 +0000

    fixed missing necessary lines, checking running once to different files

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Unittests for the console
+"""
+import unittest
+import os
+import models
+from console import HBNBCommand
+
+# TO DO: write console unittests
+
+
+if __name__ == "__main__":
+    unittest.main()
