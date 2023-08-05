32ddc17938b545b0c6aa664cc6b4cd41ba3bf889
ELCBAh <6211@holbertonstudents.com>
2023-08-05 22:43:00 +0000

    handling storage in a different way trying to fix checker errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from os import getenv
 
 
-storage_t = getenv('HBNB_TYPE_STORAGE')
+storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if storage_t == "db":
+if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
