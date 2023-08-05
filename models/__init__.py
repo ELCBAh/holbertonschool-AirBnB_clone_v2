05aecdaf47940de07881c8d38788bf267928634b
ELCBAh <6211@holbertonstudents.com>
2023-08-05 20:51:44 +0000

    fixed some syntax errors in codebase and updated models.__init__ package to handle dbstorage and filestorage correctly

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 from os import getenv
 
 
 storage_t = getenv('HBNB_TYPE_STORAGE')
 
-if storage_t is not "db":
-    storage = FileStorage()
-    storage.reload()
-else:
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
