044f7be4b61235f304f5458649e8befdff83f4c8
ELCBAh <6211@holbertonstudents.com>
2023-08-19 20:14:28 +0000

    updated both engine scripts to handle requirements and updated state file to return list of city objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,7 @@ class FileStorage:
             if key in self.all():
                 del self.all()[key]
             self.save()
+
+    def close(self):
+        """Close function calling reload"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -11,3 +13,16 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Returns a list of City instances with state_id equals to the
+            current State.id
+            """
+            from models import storage
+            list_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
