2588cc220aa7552f8da72476fdd99e2c953227a3
ELCBAh <6211@holbertonstudents.com>
2023-08-19 19:44:13 +0000

    added pyhton text function to return Python followed by text variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Flask web app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbtn_hello():
+    """returning hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returning HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """returning C"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """returning Python"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
